a2b1e1993c79ea5d594912413d7ba0d833af7e3e
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2024-12-14 21:56:35 +0200

    improving styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,38 @@
 let expanded = false;
 
 function Expand() {
-    let body = document.getElementsByTagName('BODY')[0];
-    let header = body.children[0];
-    let nav = header.children[0];
-    let ol = nav.children[0];
-    for (let i = 1; i < ol.children.length; i++) {
-        if (expanded) {
-            ol.children[i].style.display = "none";
-        } else {
-            ol.children[i].style.display = "block";
-        }
+    let header = document.getElementsByTagName('header')[0];
+    let div = document.getElementById('navCollapse');
+
+    if (expanded) {
+        header.style.marginBottom = "0";
+        div.style.display = "none";
+    } else {
+        header.style.marginBottom = "20%";
+        div.style.display = "block";
     }
+
     expanded = !expanded;
 }
+
+window.addEventListener("resize", () => {
+    let div = document.getElementById('navCollapse');
+
+    if (window.innerWidth > 991) {
+        div.style.display = "flex";
+    } else {
+        expanded = true;
+        Expand();
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    var background = document.getElementsByClassName('background')[0];
+    var shark = background.children[0];
+
+    for (var i = 0; i < 200; i++) {
+        var div = document.createElement('div');
+        div.classList.add('bubble');
+        background.insertBefore(div, shark);
+    }
+})
